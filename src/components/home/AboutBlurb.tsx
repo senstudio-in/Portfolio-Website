ff1cebb2a18544dc3a31b8ba0128056a45befde0
senstudio-in <senstudio.in@gmail.com>
2026-09-21 22:29:15 +0530

    Refine homepage biography

--- src/components/home/AboutBlurb.tsx
+++ src/components/home/AboutBlurb.tsx
@@ -5,7 +5,7 @@ import { ScrollWords } from "../Reveal";
 import { images } from "@/lib/site";
 
 const bio =
-  "Arghya Sen is a Bengaluru Based Artist, Originally From West Bengal. He Specializes in Visual Storytelling through multi-disciplinary Art Forms , Creating Content for Brands, Artists, and Creative Projects.";
+  "Arghya Sen is a Bengaluru-based art director and visual storyteller. He brings photography, videography, design and post-production together to build distinctive worlds for brands, artists and culture-led projects.";
 
 export default function AboutBlurb() {
   return (
